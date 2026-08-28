db064024addf96ab0ef4a9a20d7ac752dd94a60c
david-pessoa <pessoa.dvlbp@gmail.com>
2026-08-28 12:24:42 -0300

    fix: utiliza useNavigate() para redirecionar o usuário para última página em que estava

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode } from 'react';
 import { useUser } from '../context/AuthContext';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import type { Role } from '../types/Roles';
 
 type PrivateRoutesProps = {
@@ -10,13 +10,19 @@ type PrivateRoutesProps = {
 
 export default function PrivateRoute({ children, acceptedRoles }: PrivateRoutesProps) {
   const { user, isLoading } = useUser();
-  //useHistory()
-
+  const navigate = useNavigate();
+  
+  //Se o usuário está carregando, exibe tela de carregamento
   if (isLoading) return <div>Carregando...</div>;
 
+  // Se não consegue obter os dados do usuário, redireciona para o login
   if (!user) return <Navigate to="/login" replace />;
 
-  if (!acceptedRoles.includes(user.role)) return <Navigate to="/home" replace />;
+  // Caso o usuário esteja logado, mas não tenha permissão para acessar a rota, redireciona ele pra última página que visitou
+  if (!acceptedRoles.includes(user.role)) {
+    navigate(-1);
+    return;
+  }
 
   return children;
 }
